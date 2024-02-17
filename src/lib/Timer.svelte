1e200be140f856785caafcf0f781e1a81d339bdd
Akartikus <sitraka.razafimamy@gmail.com>
2024-02-17 23:00:07 +0100

    Init timer component

---
+++ src/lib/Timer.svelte
@@ -0,0 +1,90 @@
+<script>
+	import { appWindow } from '@tauri-apps/api/window';
+
+	// Import necessary dependencies
+	import { onMount, onDestroy } from 'svelte';
+
+	// Define component logic
+	let pomodoroDuration = 10;
+	let breakDuration = 5;
+
+	let timeLeft = pomodoroDuration;
+	let timer = 0;
+	let inBreak = false;
+	let inPlay = false;
+	// Function to start the timer
+
+	function startTimer() {
+		timer = setInterval(() => {
+			if (inPlay && timeLeft > 0) {
+				timeLeft--;
+			} else {
+				if (inPlay && timeLeft <= 0) {
+					if (inBreak) {
+						console.warn('in brak', timeLeft);
+						appWindow.toggleMaximize();
+						reset();
+					} else {
+						console.warn('in not in break');
+						inBreak = true;
+						timeLeft = breakDuration;
+						appWindow.toggleMaximize();
+						//autoplay break
+					}
+				} //else {
+				//@ts-checkrval(timer);
+				//}
+			}
+		}, 1000);
+	}
+
+	// Function to pause the timer
+	function pauseTimer() {
+		inPlay = false;
+		clearInterval(timer);
+	}
+
+	// Function to resume the timer
+	function resumeTimer() {
+		inPlay = true;
+		startTimer();
+	}
+
+	// Function to toggle play/pause
+	function playOrPause() {
+		if (inPlay) {
+			inPlay = false;
+			clearInterval(timer);
+			timer = 0;
+		} else {
+			inPlay = true;
+			startTimer();
+		}
+	}
+
+	function reset() {
+		timeLeft = pomodoroDuration;
+		clearInterval(timer);
+		inBreak = false;
+		inPlay = false;
+	}
+
+	// Lifecycle hook to start the timer when the component is mounted
+	//onMount(startTimer);
+
+	// Lifecycle hook to clear the timer when the component is destroyed
+	onDestroy(() => clearInterval(timer));
+</script>
+
+<!-- Component markup -->
+<div>
+	{#if inPlay}
+		<h2>{inBreak ? 'Well-deserved break' : 'Stay focus'}</h2>
+	{:else}
+		<h2>Start your timer</h2>
+	{/if}
+
+	<button on:click={playOrPause}>{inPlay ? 'Pause' : 'Play'}</button>
+	<button on:click={() => (timeLeft = 60)}>Reset</button>
+	<p>Time Left: {timeLeft}</p>
+</div>
