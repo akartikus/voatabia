2354d38da24420f4e35f691967643a492603382e
akartikus <sitraka.razafimamy@gmail.com>
2024-02-20 21:16:15 +0100

    Add skeleton toolkit

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config}*/
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {}
+	},
+
+
+plugins: [
+	skeleton({
+		themes: { preset: [ "skeleton" ] }
+	})
+]
+};
+
+module.exports = config;
